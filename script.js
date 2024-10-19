e8a59ffb44a3dff0c36d91e2e0271d6d10e6d9b6
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2024-10-19 13:59:08 +0300

    DATATYPES

---
+++ script.js
@@ -0,0 +1,17 @@
+//DATA TYPES IN JS
+var name = "VIVIAN"//string
+var age = 14
+var isStudent=true
+var x;
+let y = null
+const arr = []
+const obj = {}
+console.log (typeof y)
+//TYPE CONVERTION
+var age= String(age)
+try{
+    date();
+}
+catch(error){
+    console.log(error.message)
+}
